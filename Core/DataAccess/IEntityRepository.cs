9a75fc70c59813475d7f85634f2cbc2443bdedb1
ugurbasarduman <ugurbasarduman@gmail.com>
2021-10-15 05:48:41 +0300

    Add project files.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.DataAccess
+{
+    //generic constraint
+    //class:referans tip
+    //new():new'lenebilir olmali
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>>filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
