aab6bcfa3683c7d2ae3df1ff8b253e98655961be
ugurbasarduman <ugurbasarduman@gmail.com>
2021-10-29 19:28:52 +0300

    tekrar

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty();
+            RuleFor(x=>x.ProductName).MinimumLength(2);
+            RuleFor(x => x.UnitPrice).NotEmpty();
+            RuleFor(x => x.UnitPrice).GreaterThan(0);
+            RuleFor(x => x.UnitPrice).GreaterThanOrEqualTo(10).When(x => x.CategoryId == 1);
+        }
+    }
+}
